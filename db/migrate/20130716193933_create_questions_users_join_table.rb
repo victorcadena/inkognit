69857cbc4023b8d6fa6e23aadd95c379296e3a8a
victorcadena <victord.1125@gmail.com>
2013-07-16 21:17:13 -0500

    Functional many to many Users_Questions

--- db/migrate/20130716193933_create_questions_users_join_table.rb
+++ db/migrate/20130716193933_create_questions_users_join_table.rb
@@ -1,14 +1,15 @@
 class CreateQuestionsUsersJoinTable < ActiveRecord::Migration
-  def up
+  def self.up
     create_table :questions_users, :id => false do |t|
       t.integer :question_id
       t.integer :user_id
     end
 
-    add_index :questions_users, [:question_id, :user_id], :unique=>true
+    add_index :questions_users, [:question_id, :user_id], :unique => true
+
   end
 
-  def down
+  def self.down
     drop_table :questions_users
   end
 end
